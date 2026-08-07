f4019e247aac79bb8cf419bf38db3c67f930e746
Gl4ukos <g.b.hellas@gmail.com>
2026-08-07 15:02:21 +0300

    Added timeout for ack_packet

--- VOYDEKKO_node/src/main.cpp
+++ VOYDEKKO_node/src/main.cpp
@@ -15,13 +15,14 @@
 
 Display_Interface display;
 Packet packet;
+int wait_for_response = 0;
+AckPacket ack_packet;
 
 void setup() {
     Serial.begin(115200);
     if(display.setup_display() != 0){
         Serial.println("Display error");
     }
-    display.setup_display();
     Serial.println("Display online.");
     display.print_to_display("Screen online.");
     delay(500);
@@ -58,7 +59,9 @@ void setup() {
 
 }
 
-int wait_for_response = 0;
+uint32_t ack_timeout = 4000;
+uint32_t ack_time_start;
+int display_hybernation_status = 1;
 
 void loop() {
  
@@ -71,32 +74,43 @@ void loop() {
         display.update_display(packet.to_string());
 
         wait_for_response = 1;
-        display.print_to_display("\nAwaiting response...");
-    }else{
-        AckPacket ack_packet;
-        while(LoRa.available()){
-            LoRa.readBytes((uint8_t*)&ack_packet, sizeof(ack_packet));
-
-            if(ack_packet.id == packet.id){
-                if(ack_packet.status == SOLID){
-                    display.update_display(packet.to_string());
-                    display.print_to_display("\nARRIVED OK.");
-                    // wait_for_response = 0;
-                    break;
-                }else if(ack_packet.status == CORRUPT){
-                    display.update_display(packet.to_string());
-                    display.print_to_display("\nARRIVED CORRUPT.");
-                    // wait_for_response = 0;
-                    break;
-                }
-            }else if(ack_packet.id > packet.id){
-                display.update_display(packet.to_string());
-                display.print_to_display("\nPACKET LOSS!");
-                // wait_for_response = 0;
-                break;
-            }
-        }
+        display.print_to_display("\nAwaiting response...");     
+        LoRa.receive();
+        ack_time_start = millis();
     }
     
-    delay(330);
+    while(wait_for_response == 1){
+        int packet_size = LoRa.parsePacket();
+        if(packet_size == sizeof(ack_packet)){
+            if(LoRa.available()){
+                LoRa.readBytes((uint8_t*)&ack_packet, sizeof(ack_packet));
+                if(ack_packet.id == packet.id){
+                    if(ack_packet.status == SOLID){
+                        display.update_display(packet.to_string());
+                        display.print_to_display("\nARRIVED OK.");
+                        delay(1000);
+                        wait_for_response = 0;
+                    }else if(ack_packet.status == CORRUPT){
+                        display.update_display(packet.to_string());
+                        display.print_to_display("\nARRIVED CORRUPT.");
+                        delay(1000);
+                        wait_for_response = 0;
+                    }
+                }else if(ack_packet.id > packet.id){
+                    display.update_display(packet.to_string());
+                    display.print_to_display("\nPACKET LOSS!");
+                    delay(1000);
+                    wait_for_response = 0;
+                }
+            }
+        }
+        if((millis() - ack_time_start) >= ack_timeout){
+            wait_for_response = 0;
+            display.update_display("No response from Base");
+        }
+        delay(50);
+    }
+    
+    display.print_to_display("Hybernating...");
+    delay(5000);
 }
